57e97d5c7c84dc47e99930afc5823cf2ea476102
ooozguuur <ozgur.isik1987@gmail.com>
2022-05-16 16:47:11 +0300

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async() => {
+
+  accounts = await web3.eth.getAccounts();
+   
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi Ozgur!']})
+    .send( {from: accounts[0], gas: '1000000' });
+
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(inbox);
+  });
+
+});
