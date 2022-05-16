5b965c3c503e7a6f49b267a64b6c4dd3a62068f1
ooozguuur <ozgur.isik1987@gmail.com>
2022-05-16 22:49:42 +0300

    added test methods

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,20 +6,33 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi Ozgur';
 
 beforeEach(async() => {
 
   accounts = await web3.eth.getAccounts();
    
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi Ozgur!']})
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING]})
     .send( {from: accounts[0], gas: '1000000' });
 
 });
 
 describe('Inbox', () => {
+
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('New Ozgur Message').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.notEqual(message, INITIAL_STRING);
   });
 
 });
